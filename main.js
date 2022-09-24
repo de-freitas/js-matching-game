796bc50514d4411c7fa318944d5eeb5cb13109c7
unknown <claudio.g.freitas@outlook.com>
2022-09-24 20:39:17 -0300

    Create BoardGame object without css

--- main.js
+++ main.js
@@ -1,33 +1,9 @@
-// import './style.css'
-// import javascriptLogo from './javascript.svg'
-// import { setupCounter } from './counter.js'
-
-// document.querySelector('#app').innerHTML = `
-//   <div>
-//     <a href="https://vitejs.dev" target="_blank">
-//       <img src="/vite.svg" class="logo" alt="Vite logo" />
-//     </a>
-//     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-//       <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-//     </a>
-//     <h1>Hello Vite!</h1>
-//     <div class="card">
-//       <button id="counter" type="button"></button>
-//     </div>
-//     <p class="read-the-docs">
-//       Click on the Vite logo to learn more
-//     </p>
-//   </div>
-// `
-
-// setupCounter(document.querySelector('#counter'))
-// ==================================================================================================== //
 import './src/styles/settings/colors.css';
 import './src/styles/generic/generic.css';
 import './src/styles/elements/base.css';
-import CardGame from './src/components/CardGame';
+import BoardGame from './src/objects/BoardGame';
 
 const $root = document.querySelector('#root');
-const $htmlCardGame = CardGame();
+const $htmlBoardGame = BoardGame(3);
 
-$root.insertAdjacentHTML('beforeend', $htmlCardGame)
+$root.insertAdjacentHTML('beforeend', $htmlBoardGame);
