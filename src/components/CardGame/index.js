009f671a4bca4877afdea391f97d92fddf1a38fa
unknown <claudio.g.freitas@outlook.com>
2022-09-24 19:52:17 -0300

    Create CardGame component

---
+++ src/components/CardGame/index.js
@@ -0,0 +1,12 @@
+import './style.css'
+
+function CardGame() {
+    return `
+        <article class="card-game">
+            <img src="src/images/alura-pixel-1.png" alt="Logo da Alura"> </img>
+        </article>
+    `   
+};
+
+
+export default CardGame;
