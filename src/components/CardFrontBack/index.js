2917ef6afe5ead044ed3e58302497a7f9462c913
unknown <claudio.g.freitas@outlook.com>
2022-09-25 12:58:16 -0300

    Add click event on CardFrontBack component.

--- src/components/CardFrontBack/index.js
+++ src/components/CardFrontBack/index.js
@@ -1,17 +1,32 @@
 import CardGame from '../CardGame'
 import './style.css'
 
+
 function CardFrontBack() {
+    window.cardFrontBack = {};
+    window.cardFrontBack.handleClick = (event) => {
+        const $origin = event.target;
+        const $cardFrontBack = $origin.closest('.card-front-back');
+
+        // convencional
+        // if ($cardFrontBack.classList.contains('-active')) {
+        //     $cardFrontBack.classList.remove('-active');
+        // } else {
+        //     $cardFrontBack.classList.add('-active');
+        // }
+
+        // substituindo pelo .toggle()
+        $cardFrontBack.classList.toggle('-active');
+    };
+
     return /*html*/`
-        <article class="card-front-back">
-            
+        <article class="card-front-back" onClick="cardFrontBack.handleClick(event)">
             <div class="card -front">
                 ${CardGame()}
             </div>
             <div class="card -back">
                 ${CardGame('logo-javascript-2', 'Logo JavaScript')}
             </div>
-
         </article>
     `;
 }
